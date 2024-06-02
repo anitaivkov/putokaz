3057a69a35cbbc8369ce2f38c68d7f278cafacf4
anitaivkov <121263796+anitaivkov@users.noreply.github.com>
2024-06-02 16:35:25 +0200

    Dodane f-cije za kreiranje dat i dodatni ispis destinacija

--- putokaz/functions.c
+++ putokaz/functions.c
@@ -1,6 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 #include "header.h"
+
+
+//kreiranje datoteke
+int create_file(const char* const file_name) {
+	FILE* fp = fopen(file_name, "ab+");
+	if (fp == NULL) {
+		perror("Greska: ");
+		return -1;
+	}
+
+	fclose(fp);
+	return 0;
+}
+
+int add_to_file(const char* const file_name) {
+
+}
+
+
+void dest_print_question() {
+	char choice = 0;
+	do {
+		printf("\nZelite li prije odabira destinacije ispisati sve dostupne destinacije? (Y/N)\n");
+		scanf("%c", &choice);
+		if (choice == 'Y' || choice == 'y') {
+			//ispis cijele datoteke koja sadrzi destinacije za putovanje --> DOPUNITI
+			printf("Ja sam potvrdni odgovor za ispis svih destinacija prije odabira neke destinacije.\n");
+		}
+	} while (choice != 'Y', choice != 'y', choice != 'N', choice != 'n');
+}
 
 
 
